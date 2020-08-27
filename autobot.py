8d4812aa197421c88301a3e5386fe96162cb3da5
Luke Babarinde <neofinder@gmail.com>
2020-08-27 01:21:07 -0400

    Modified autobot to include scraping

--- autobot.py
+++ autobot.py
@@ -1,95 +1,98 @@
-from sys import argv
 import time
 import os
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 import sys, getopt
+from selenium.webdriver.remote.webelement import WebElement
+from bs4 import BeautifulSoup
 import validators
+import pandas as pd
 
 def main():
     url = ''
     trials = ''
-    driver_path = ''
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"H:t:d:")
+        opts, args = getopt.getopt(sys.argv[1:],"u:t:")
         for opt, arg in opts:
-            if opt == '-H':
+            if opt == '-u':
                 url = str(arg)
-                if validators.url(url):
-                    continue
-                else:
-                    print ("Enter a FQDN host e.g. http://domain.com")
-                    return
             elif opt == '-t':
                 try:
                     trials = int(arg)
                 except ValueError:
                     print ("Enter a valid number for trials")
-            elif opt == '-d':
-                driver_path = str(arg)
     except getopt.GetoptError as e:
-        print (e, 'python autosurf.py -H <Host> -t <No of Trials> -d <selenium driver path>')
+        print (e, 'python wafautosurf.py -u <Host URL> -t <No of Trials>')
         sys.exit(2)
-    return url, trials, driver_path
 
-    # #Start automated browsing after validating the url
+    #Start automated browsing after validating the url
+    if validators.url(url) and trials > 0:
+        auto_surf(url, trials)
+    else:
+        print ("Check the innput parameters for url and no. of trials")
+        print (e, 'python wafautosurf.py -u <Host URL> -t <No of Trials>')
 
-    # if validators.url(url) and trials > 0:
-    #     auto_surf(url, trials)
-    # else:
-    #     print ("Check the innput parameters for url and no. of trials")
-    #     print ('python autosurf.py -H <Host> -i <No of Trials>')
-
-def set_driver(url, driver_path):
+def set_driver():
     option = webdriver.ChromeOptions()
     option.add_argument("-incognito")
-    print (driver_path)
-    browser = webdriver.Chrome(executable_path="./chromedriver", options=option)
-    print (f'{browser}')
-    # print ("set",driver_path)
-    # browser = webdriver.Chrome(executable_path=path+"/chromedriver", options=option)
-    # # browser.get(url)
-    # return browser
+    try:
+        '''Provide the path for the driver that is required to connect with local broswer
+        By default, it is assumed that Chrome is install on the test machine.
+        With driver downloaded from https://chromedriver.chromium.org/
+        and placed inside /usr/bin/local on a mac. For Windows, simply change the path'''
+        browser = webdriver.Chrome(executable_path="./drivers/chromedriver", options=option)
+        return browser
+    except:
+        print ("Place driver for browser in /usr/local/bin. This can be downloaded from https://chromedriver.chromium.org/")
+        return
+
+def login(url):
+    driver = set_driver()
+    driver.get (url)
+    try:
+        time.sleep(2)
+        driver.find_element_by_name('username').send_keys("random")
+        element = driver.find_element_by_name('password')
+        element.send_keys('bad_password')
+        element.send_keys(Keys.RETURN)
+        driver.close()
+    except:
+        time.sleep(1) #slight delay to closely simulate a real browser
+        print(driver.get_network_conditions)
+        driver.close()
+        return
+
+def humanbot(url):
+    driver = set_driver()
+    driver.get(url)
+
+    time.sleep(1)
+    element = driver.find_element_by_name('ticker')
+    element.send_keys("AAPL")
+    element.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    driver.quit()
+
+    # Find table on the page and start scraping
+    tab_data = soup.select('table')[1]
+    n = 0
+    item = []
+    while n <= 5:
+        for items in tab_data.select('tr'):
+            item = [elem.text for elem in items.select('th,td')]
+            item.append
+        n += 1
+        print (item)
+        # print(' '.join(item))
 
 
-    # specify_path = str(input("Enter Selenium driver full path :"))
-    # driver_path = driver_path
-    # try:
-    #     '''Provide the path for the driver that is required to connect with local broswer
-    #     By default, it is assumed that Chrome is install on the test machine.
-    #     With driver downloaded from https://chromedriver.chromium.org/
-    #     and placed inside /usr/bin/local on a mac. For Windows, simply change the path'''
-        
-    #     browser = webdriver.Chrome(executable_path=driver_path, options=option)
-    #     return browser
-    #     # print (browser)
-    # except:
-    #     print ("Place driver for browser in /usr/local/bin. This can be downloaded from https://chromedriver.chromium.org/")
-    #     return
-
-def login(url, driver_path):
-    driver_path = f"""\"{driver_path}\""""
-    driver = set_driver(url, driver_path)
-    # set_driver(url, driver_path)
-    # print ("test", driver)
-    # driver.get(url)
-    # try:
-    #     time.sleep(2)
-    #     driver.find_element_by_name('username').send_keys("random")
-    #     element = driver.find_element_by_name('password')
-    #     element.send_keys('bad_password')
-    #     element.send_keys(Keys.RETURN)
-    #     driver.close()
-    # except:
-    #     time.sleep(1) #slight delay to closely simulate a real browser
-    #     print(driver.get_network_conditions)
-    #     driver.close()
-    #     return
-
-def auto_surf(url, trials, driver_path):
+def auto_surf(url, trials):
     n = 1
     while n <= trials:
-        login(url, driver_path)
+        # login(url)
+        humanbot(url)
         n += 1
         print (f'Round {n-1} of {trials} automated browsing of {url}')
 
@@ -112,6 +115,5 @@ def cred_spray():
             print (params)
 
 if __name__ == "__main__":
-    url, trial, driver_path = main()
-    auto_surf(url, trial, driver_path)
+    main()
     # cred_spray() #Still under development. Will introduce multithreading
