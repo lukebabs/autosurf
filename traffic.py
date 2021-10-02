db8fa7d7634f37e7cd077a5d9d7fdc4b25bf4bcb
Luke Babarinde <neofinder@gmail.com>
2021-10-01 22:29:22 -0400

    Update traffic.py

--- traffic.py
+++ traffic.py
@@ -76,14 +76,14 @@ def start_requests(sites, i):
 #Creating multiple threads to scale the events generated
 def load_threading(sites):
     r1 = threading.Thread(target=start_requests, args=(random.choice(sites), 0))
-    r2 = threading.Thread(target=start_requests, args=(random.choice(sites), 0))
-    r3 = threading.Thread(target=start_requests, args=(random.choice(sites), 0))
-    r4 = threading.Thread(target=start_requests, args=(random.choice(sites), 0))
+    # r2 = threading.Thread(target=start_requests, args=(random.choice(sites), 0))
+    # r3 = threading.Thread(target=start_requests, args=(random.choice(sites), 0))
+    # r4 = threading.Thread(target=start_requests, args=(random.choice(sites), 0))
     try:
         r1.start()
-        r2.start()
-        r3.start()
-        r4.start()
+        # r2.start()
+        # r3.start()
+        # r4.start()
     except Exception as e:
         print (e)
     return
